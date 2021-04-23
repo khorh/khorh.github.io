8638f1732251be5cbb8f405ad74a53b02319d939
khorh <helen.khor@googlemail.com>
2021-04-23 10:16:08 +0100

    added animation to atom project

---
+++ animations.js
@@ -0,0 +1,5 @@
+// Atom
+document.getElementById('atomPlayPause').addEventListener('click', () => {
+    document.getElementById('atomAnimation').innerHTML += '<circle class="electron-one electronAnimation" r="5" cx="50%" cy="20%" /><circle class="electron-two electronAnimation" r="5" cx="80%" cy="76%" />'
+});
+
