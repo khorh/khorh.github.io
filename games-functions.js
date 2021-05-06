76cd8082daafabd47695ce6ffb3577ba14acc8c5
khorh <helen.khor@googlemail.com>
2021-05-06 19:34:58 +0100

    refactored code

--- games-functions.js
+++ games-functions.js
@@ -2,26 +2,6 @@
 
 // Coin toss
 /**
- * function to randomise the toss of a coin
- * @returns a Peter or Benjamin coin image
- */
-const displayToss = () => {
-    let coinToss = Math.ceil(Math.random() * 2);
-
-    if (coinToss === 1) {
-        document.querySelector('#coinToss').setAttribute('src', 'images/games/coin-toss-peter.png');
-        document.querySelector('#coinToss').setAttribute('alt', 'peter rabbit 50p coin');
-        numberOfPeter += 1;
-        document.getElementById('totalPeter').innerHTML = `Peter: ${numberOfPeter}`;
-    } else if (coinToss === 2) {
-        document.querySelector('#coinToss').setAttribute('src','images/games/coin-toss-benjamin.png');
-        document.querySelector('#coinToss').setAttribute('alt', 'benjamin rabbit 50p coin');
-        numberOfBenjamin +=1;
-        document.getElementById('totalBenjamin').innerHTML = `Benjamin: ${numberOfBenjamin}`;
-    }
-}
-
-/**
  * function to add one to every click of the Toss! button
  * @returns a total number of tosses
  */
@@ -30,124 +10,200 @@ const displayTotalTosses = () => {
     document.getElementById('totalTosses').innerHTML = `Total tosses: ${numberOfTosses}`;
 }
 
+/**
+ * function to randomise the toss of a coin
+ * @returns a Peter or Benjamin coin image
+ */
+const displayToss = () => {
+    const coinToss = Math.ceil(Math.random() * 2);
+
+    if (coinToss === 1) {
+        document.querySelector('#coinTossImage').setAttribute('src', 'images/games/coin-toss-peter.png');
+        document.querySelector('#coinTossImage').setAttribute('alt', 'peter rabbit 50p coin');
+        numberOfPeter += 1;
+        document.getElementById('totalPeter').innerHTML = `Peter: ${numberOfPeter}`;
+    } else if (coinToss === 2) {
+        document.querySelector('#coinTossImage').setAttribute('src','images/games/coin-toss-benjamin.png');
+        document.querySelector('#coinTossImage').setAttribute('alt', 'benjamin rabbit 50p coin');
+        numberOfBenjamin +=1;
+        document.getElementById('totalBenjamin').innerHTML = `Benjamin: ${numberOfBenjamin}`;
+    }
+}
+
 // Roshambo
+const getPlayerSelection = () => {
+    let playerRandomNumber = Math.ceil(Math.random() * 3);
 
-/**
- * function for player to show the image and alt text of rock, paper or scissors
- * @returns the image and alt text of rock, paper or scissors
- */
-const displayPlayerRock = () => {
-    document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-rock.png');
-    document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a rock sign');
-    return 'rock';
+    switch (playerRandomNumber) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
 }
 
-const displayPlayerPaper = () => {
-    document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-paper.png');
-    document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a paper sign');
-    return 'paper';
-}
-
-const displayPlayerScissors = () => {
-    document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-scissors.png');
-    document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a scissors sign');
-    return 'scissors';
-}
-
-/**
- * function to randomise the player's selection of rock, paper or scissors
- * @returns the image and alt text related to the random selection
- */
 const displayPlayerSelection = () => {
-    const playerRandomNumber = Math.ceil(Math.random() * 3);
-    if (playerRandomNumber === 1) {
-        return displayPlayerRock();
-    } else if (playerRandomNumber === 2) {
-        return displayPlayerPaper();
-    } else if (playerRandomNumber === 3) {
-        return displayPlayerScissors();
-    }
-}
-
-/**
- * function for computer to show the image and alt text of rock, paper or scissors
- * @returns the image and alt text of rock, paper or scissors
- */
-const displayComputerRock = () => {
-    document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-rock.png');
-    document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a rock sign');
-    return 'rock';
-}
-
-const displayComputerPaper = () => {
-    document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-paper.png');
-    document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a paper sign');
-    return 'paper';
-}
-
-const displayComputerScissors = () => {
-    document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-scissors.png');
-    document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a scissors sign');
-    return 'scissors';
-}
-
-/**
- * function to randomise the computer's selection of rock, paper or scissors
- * @returns the image and alt text related to the random selection
- */
-const displayComputerSelection = () => {
-    const computerRandomNumber = Math.ceil(Math.random() * 3);
-    if (computerRandomNumber === 1) {
-        return displayComputerRock();
-    } else if (computerRandomNumber === 2) {
-        return displayComputerPaper();
-    } else if (computerRandomNumber === 3) {
-        return displayComputerScissors();
-    }
-}
-
-/**
- * functions to provide text based on the result between player and computer
- * @returns the text in the paragraph tag
- */
-const drawResult = () => {
-    document.getElementById('roshamboResult').textContent = 'It is a draw. Try again?';
-    document.querySelector('#playerWinGif').removeAttribute('src');
-    document.querySelector('#playerWinGif').removeAttribute('alt');
-}
-
-const playerWinResult = () => {
-    document.getElementById('roshamboResult').textContent = 'You win! Have another go?';
-    document.querySelector('#playerWinGif').setAttribute('src','https://media.giphy.com/media/VxbvpfaTTo3le/giphy.gif');
-    document.querySelector('#playerWinGif').setAttribute('alt','gif image of kitten flying in the air');
-}
-
-const computerWinResult = () => {
-    document.getElementById('roshamboResult').textContent = 'Computer beat you this time! Have another go?';
-    document.querySelector('#playerWinGif').removeAttribute('src');
-    document.querySelector('#playerWinGif').removeAttribute('alt');
-}
-
-/**
- * functions to determine the result of the roshambo
- * @returns the text in the paragraph tag
- */
-const displayRoshamboResult = () => {
-    if (displayPlayerSelection() === displayComputerSelection()) {
-        return drawResult();
-    } else if (displayPlayerSelection() === 'rock' && displayComputerSelection() === 'scissors') {
-        return playerWinResult();
-    } else if (displayPlayerSelection() === 'rock' && displayComputerSelection() === 'paper') {
-        return computerWinResult();
-    } else if (displayPlayerSelection() === 'paper' && displayComputerSelection() === 'rock') {
-        return playerWinResult();
-    } else if (displayPlayerSelection() === 'paper' && displayComputerSelection() === 'scissors') {
-        return computerWinResult();
-    } else if (displayPlayerSelection() === 'scissors' && displayComputerSelection() === 'rock') {
-        return computerWinResult();
-    } else if (displayPlayerSelection() === 'scissors' && displayComputerSelection() === 'paper') {
-        return playerWinResult();
+    if (getPlayerSelection() === 'rock') {
+            document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-rock.png');
+            document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a rock sign');
+    } else if (getPlayerSelection() === 'paper') {
+            document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-paper.png');
+            document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a paper sign');
     } else {
-        return 'Try again!';
+            document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-scissors.png');
+            document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a scissors sign');
     }
 }
+
+const getComputerSelection = () => {
+    let computerRandomNumber = Math.ceil(Math.random() * 3);
+
+    switch (computerRandomNumber) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
+}
+
+const displayComputerSelection = () => {
+    if (getComputerSelection() === 'rock') {
+        document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-rock.png');
+        document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a rock sign');
+    } else if (getComputerSelection() === 'paper') {
+        document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-paper.png');
+        document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a paper sign');
+    } else {
+        document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-scissors.png');
+        document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a scissors sign');
+    }
+}
+
+const displayRoshamboResult = () => {
+    if (getPlayerSelection === getComputerSelection) {
+        document.getElementById('roshamboResult').textContent = 'It is a draw. Try again?';
+    }
+    if (getPlayerSelection === 'rock') {
+        if (getComputerSelection === 'paper') {
+            document.getElementById('roshamboResult').textContent = 'Computer beat you this time! Have another go?';
+        } else {
+            document.getElementById('roshamboResult').textContent = 'You win! Have another go?';
+        }
+    }
+    if (getPlayerSelection === 'paper') {
+        if (getComputerSelection === 'scissors') {
+            document.getElementById('roshamboResult').textContent = 'You win! Have another go?';
+        } else {
+            document.getElementById('roshamboResult').textContent = 'Computer beat you this time! Have another go?';
+        }
+    }
+    if (getPlayerSelection === 'scissors') {
+        if (getComputerSelection === 'rock') {
+            document.getElementById('roshamboResult').textContent = 'Computer beat you this time! Have another go?';
+        } else {
+            document.getElementById('roshamboResult').textContent = 'You win! Have another go?';
+        }
+    }
+}
+
+// console.log(displayRoshamboResult());
+
+// Roshambo Original
+// /**
+//  * function to randomise the player's selection of rock, paper or scissors
+//  * @returns the image and alt text related to the random selection
+//  */
+
+// console.log(playerRandomNumber);
+// const displayPlayerSelection = () => {
+//     let playerRandomNumber = Math.ceil(Math.random() * 3);
+//
+//     if (playerRandomNumber === 1) {
+//         document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-rock.png');
+//         document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a rock sign');
+//     } else if (playerRandomNumber === 2) {
+//         document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-paper.png');
+//         document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a paper sign');
+//     } else if (playerRandomNumber === 3) {
+//         document.querySelector('#playerSelection').setAttribute('src', 'images/games/roshambo-scissors.png');
+//         document.querySelector('#playerSelection').setAttribute('alt', 'paw of cat showing a scissors sign');
+//     }
+// }
+
+// /**
+//  * function to randomise the computer's selection of rock, paper or scissors
+//  * @returns the image and alt text related to the random selection
+//  */
+//
+// const computerRandomNumber = Math.ceil(Math.random() * 3);
+// console.log(computerRandomNumber);
+
+// let displayComputerSelection = () => {
+//     if (computerRandomNumber === 1) {
+//         document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-rock.png');
+//         document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a rock sign');
+//         return 'rock';
+//     } else if (computerRandomNumber === 2) {
+//         document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-paper.png');
+//         document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a paper sign');
+//         return 'paper';
+//     } else if (computerRandomNumber === 3) {
+//         document.querySelector('#computerSelection').setAttribute('src', 'images/games/roshambo-scissors.png');
+//         document.querySelector('#computerSelection').setAttribute('alt', 'paw of cat showing a scissors sign');
+//         return 'scissors';
+//     }
+// }
+
+// /**
+//  * functions to provide text based on the result between player and computer
+//  * @returns the text in the paragraph tag
+//  */
+// const drawResult = () => {
+//     document.getElementById('roshamboResult').textContent = 'It is a draw. Try again?';
+//     document.querySelector('#playerWinGif').removeAttribute('src');
+//     document.querySelector('#playerWinGif').removeAttribute('alt');
+// }
+//
+// const playerWinResult = () => {
+//     document.getElementById('roshamboResult').textContent = 'You win! Have another go?';
+//     document.querySelector('#playerWinGif').setAttribute('src','https://media.giphy.com/media/VxbvpfaTTo3le/giphy.gif');
+//     document.querySelector('#playerWinGif').setAttribute('alt','gif image of kitten flying in the air');
+// }
+//
+// const computerWinResult = () => {
+//     document.getElementById('roshamboResult').textContent = 'Computer beat you this time! Have another go?';
+//     document.querySelector('#playerWinGif').removeAttribute('src');
+//     document.querySelector('#playerWinGif').removeAttribute('alt');
+// }
+
+// /**
+//  * functions to determine the result of the roshambo
+//  * @returns the text in the paragraph tag
+//  */
+// const roshamboResult = () => {
+//     if (playerRandomNumber === computerRandomNumber) {
+//         return 'draw';
+//     } else if (playerRandomNumber === 1 && computerRandomNumber === 2) {
+//         return 'computer wins';
+//     } else if (playerRandomNumber === 1 && computerRandomNumber === 3) {
+//         return 'player wins';
+//     } else if (playerRandomNumber === 2 && computerRandomNumber === 1) {
+//         return 'player wins';
+//     } else if (playerRandomNumber === 2 && computerRandomNumber === 3) {
+//         return 'computer wins';
+//     } else if (playerRandomNumber === 3 && computerRandomNumber === 1) {
+//         return 'computer wins';
+//     } else if (playerRandomNumber === 3 && computerRandomNumber === 2) {
+//         return 'player wins';
+//     }
+//     else {
+//         return 'Something else!';
+//     }
+// }
+//
+//
+// console.log(roshamboResult());
--- tools.js
+++ tools.js
@@ -1,8 +1,6 @@
-document.getElementById('calculate').addEventListener('click', () => {
-
-    const validateLoanAmount = () => {
-        
-    }
+// Loan calculator
+// Calculate loan
+document.getElementById('calculateLoan').addEventListener('click', () => {
 
     const getLoanAmount = parseInt(document.getElementById('loan').value);
     // console.log(typeof getLoanAmount);
@@ -45,3 +43,87 @@ document.getElementById('calculate').addEventListener('click', () => {
     const lastRepaymentAmount = repaymentPerMonth + (totalAmount - (repaymentPerMonth * lengthOfRepayment));
     document.getElementById('lastRepaymentAmount').innerHTML = `${lastRepaymentAmount}`;
 });
+
+// Reset loan calculator
+document.getElementById('resetLoanCal').addEventListener('click', () => {
+    document.getElementById('loan').value = '';
+    document.getElementById('salary').value = '25000';
+    document.getElementById('repayment').value = '10';
+})
+
+
+// Pair Pro
+// P1 name and avatar
+document.getElementById('personOne').addEventListener('keyup',() => {
+    const getPersonOne = document.getElementById('personOne').value;
+    document.getElementById('personOneName').textContent = `${getPersonOne}`;
+});
+
+document.getElementById('catAvatar').addEventListener('click', () => {
+    document.getElementById('personOneAvatar').setAttribute('src','images/tools/cat.svg');
+});
+
+document.getElementById('dogAvatar').addEventListener('click', () => {
+    document.getElementById('personOneAvatar').setAttribute('src','images/tools/dog.svg');
+});
+
+document.getElementById('rabbitAvatar').addEventListener('click', () => {
+    document.getElementById('personOneAvatar').setAttribute('src','images/tools/rabbit.svg');
+});
+
+// P2 name and avatar
+document.getElementById('personTwo').addEventListener('keyup', () => {
+    const getPersonTwo = document.getElementById('personTwo').value;
+    document.getElementById('personTwoName').textContent = `${getPersonTwo}`;
+});
+
+document.getElementById('chimpanzeeAvatar').addEventListener('click', () => {
+    document.getElementById('personTwoAvatar').setAttribute('src','images/tools/chimpanzee.svg');
+});
+
+document.getElementById('giraffeAvatar').addEventListener('click', () => {
+    document.getElementById('personTwoAvatar').setAttribute('src','images/tools/giraffe.svg');
+});
+
+document.getElementById('lemurAvatar').addEventListener('click', () => {
+    document.getElementById('personTwoAvatar').setAttribute('src','images/tools/lemur.svg');
+});
+
+// Start pair programming
+document.getElementById('startPairPro').addEventListener('click', () => {
+    let getTotalCodingTime = parseInt(document.getElementById('totalCodingTime').value);
+    let halfTotalCodingTime = getTotalCodingTime / 2;
+
+    // Starting position
+    document.getElementById('startPairPro').disabled = true;
+    document.getElementById('personOneCountdown').innerHTML = `${getTotalCodingTime} minutes left`;
+    document.getElementById('personTwoCountdown').innerHTML = `Please observe`;
+
+    // Countdown of total coding time, P1 and P2
+    setInterval(() => {
+        getTotalCodingTime--;
+        document.getElementById('personOneCountdown').innerHTML = `${getTotalCodingTime} minutes left`;
+
+        if (getTotalCodingTime <= halfTotalCodingTime) {
+            document.getElementById('personOneCountdown').innerHTML = `Next person`;
+        }
+
+        if (getTotalCodingTime > 0 && getTotalCodingTime <= halfTotalCodingTime) {
+            document.getElementById('personTwoCountdown').innerHTML = `${getTotalCodingTime} minutes left`;
+        }
+
+        if (getTotalCodingTime <= 0) {
+            clearInterval();
+            document.getElementById('personOneCountdown').innerHTML = `Have a break!`;
+            document.getElementById('personTwoCountdown').innerHTML = `Have a break!`;
+            document.getElementById('startPairPro').disabled = false;
+        }
+    }, 1000);
+})
+
+// Reset pair programming
+document.getElementById('resetPairPro').addEventListener('click', () => {
+    document.getElementById('personOne').value = '';
+    document.getElementById('personTwo').value = '';
+    document.getElementById('totalCodingTime').value = '';
+})
